1a4f42aecbb54bed271fb72f6cc4478cb1d3ad31
worldguigui <1492735932@qq.com>
2026-06-08 22:32:55 +0800

    完成了配置读取、虚拟时钟的暂停逻辑

--- src/main/java/com/own/virtualaibox/grid/GridTest.java
+++ src/main/java/com/own/virtualaibox/grid/GridTest.java
@@ -2,17 +2,20 @@ package com.own.virtualaibox.grid;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 public class GridTest {
     private static final int width = 10;
     private static final int height = 10;
     private Map<String,Integer> id2Block;
-    private List<Integer> ids;
+    private Map<Integer,List<String>> block2Ids;
+    private List<String> ids;
+    private static final int[][] directions = new int[][]{
+            {-1,-1},{-1,0},{-1,1},
+            {0,-1},{0,0},{1,-1},
+            {1,0},{1,0},{1,1}
+    };
 
     /**
      * 0 - 默认初始化
@@ -22,7 +25,8 @@ public class GridTest {
      */
     public GridTest(int option) {
         id2Block = new HashMap<String,Integer>();
-        ids = new ArrayList<Integer>();
+        ids = new ArrayList<String>();
+        block2Ids = new HashMap<>();
 
         switch(option) {
             case 0:
@@ -30,11 +34,14 @@ public class GridTest {
                 break;
                 case 1:
                     for(int i = 0; i < 3; i++) {
-                        ids.add(i);
+                        ids.add(String.valueOf(i));
                     }
                     id2Block.put("1",25);
                     id2Block.put("2",25);
                     id2Block.put("3",50);
+
+                    block2Ids.put(25,List.of("1","2"));
+                    block2Ids.put(50,List.of("3"));
                     break;
                     default:
                         log.info("网格初始化成功，当前网格共有0人");
@@ -43,10 +50,62 @@ public class GridTest {
 
     public GridTest() {
         id2Block = new HashMap<String,Integer>();
-        ids = new ArrayList<Integer>();
+        ids = new ArrayList<String>();
+        block2Ids = new HashMap<>();
     }
 
-    public List<Integer> getIds() {
+    /**
+     * 返回所有人物
+     * @return
+     */
+    public List<String> getIds() {
         return ids;
+    }
+
+    /**
+     * 获取当前id附近的人物
+     * @param id
+     * @return
+     */
+    public List<String> getNearby(String id) {
+        int curPos = id2Block.getOrDefault(id,-1);
+
+        if (curPos == -1) {
+            return null;
+        }
+
+        List<String> nearbyIds = new ArrayList<>();
+        for(int i = 0; i < directions.length; i++) {
+            int nearby = curPos + width * directions[i][0] + directions[i][1];
+
+            if(nearby < 0 || nearby >= width * height) {
+                continue;
+            }
+
+            // 检索临近格子nearby里的所有人物id
+            if(block2Ids.containsKey(nearby)) {
+                for(String nearbyId : block2Ids.get(nearby) ) {
+                    nearbyIds.add(nearbyId);
+                }
+            }
+
+        }
+
+        return nearbyIds;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for(Integer block : block2Ids.keySet()) {
+            sb.append(block + ": ");
+            for(String id : block2Ids.get(block)) {
+                sb.append(id + " ");
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
     }
 }
--- src/test/java/com/own/virtualaibox/VirtualAiBoxApplicationTests.java
+++ src/test/java/com/own/virtualaibox/VirtualAiBoxApplicationTests.java
@@ -1,15 +1,27 @@
 package com.own.virtualaibox;
 
 import com.own.virtualaibox.grid.GridTest;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
 @SpringBootTest
 class VirtualAiBoxApplicationTests {
 
     @Test
     void contextLoads() {
-        GridTest gridTest = new GridTest();
+        GridTest gridTest = new GridTest(1);
+
+        List<String> ids = gridTest.getNearby("1");
+
+        for (String id : ids) {
+            log.info(id);
+        }
+
     }
 
 }
